6ed6610039a01331c0003fe6c6921affc27d7128
Martin <martin.blascak86@gmail.com>
2022-07-01 09:01:47 +0200

    editing the home and search views

--- src/views/SearchView.vue
+++ src/views/SearchView.vue
@@ -1,5 +1,30 @@
 <template>
-  <div class="about">
-
-  </div>
+	<div class="search">
+		<InputSearch />
+		<PicturesTemplate :images="collection" />
+	</div>
 </template>
+
+
+<script>
+import {mapFields} from "vuex-map-fields"
+import PicturesTemplate from "@/components/PicturesTemplate.vue";
+import InputSearch from "@/components/InputSearch.vue";
+
+export default {
+	name: "SearchView",
+	components: {
+		PicturesTemplate,
+		InputSearch,
+	},
+	data: () => ({
+
+	}),
+	computed: {
+		...mapFields("search", {
+			collection: "collection",
+		}),
+	}
+}
+
+</script>
